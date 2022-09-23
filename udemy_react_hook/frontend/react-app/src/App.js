0ff4bc023f0dbeb6e8d4241cc161b6dc790d325c
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-23 18:23:59 +0900

    template

--- udemy_react_hook/frontend/react-app/src/App.js
+++ udemy_react_hook/frontend/react-app/src/App.js
@@ -1,29 +1,9 @@
 import React, { useState, useEffect } from "react"
 
 const App = (props) =>  {
-  const [state, setState] = useState(props)
-  const { name, price } = state
-  useEffect(() => {
-    console.log("This callback is from name only")
-  },[name])
 
   return (
     <>
-      {/* // objectを渡す{} */}
-      {/* // stateを渡してあげる{...state,} */}
-
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-      <button onClick={() => setState(props)}>reset</button>
-      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   );
 }
-
-App.defaultProps = {
-  name: 'サンプル',
-  price: 1000
-}
-
-export default App;
