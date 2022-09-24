a08b90433f01e54846ce202fc419b7f833386fd5
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-24 21:28:48 +0900

    implement event creation

--- udemy_react_hook/frontend/react-app/src/components/App.js
+++ udemy_react_hook/frontend/react-app/src/components/App.js
@@ -1,7 +1,24 @@
-import React, { useState, useEffect } from "react"
+import React, { useState,  useReducer } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
-const App = () =>  {
 
+import reducer from '../reducers'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('')
+    setBody('')
+  }
+  console.log({state})
   return (
     <>
       <div className="container-fluid">
@@ -9,15 +26,15 @@ const App = () =>  {
         <form>
           <div className="form-group">
             <label htmlFor="formEventTitle">タイトル</label>
-            <input className="form-control" id="formEventTitle" />
+            <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
           </div>
 
           <div className="form-group">
             <label htmlFor="formEventBody">ボディー</label>
-            <textarea className="form-control" id="formEventBody" />
+            <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
           </div>
 
-          <button className="btn btn-primary">イベントを作成する</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">全てのイベントを削除する</button>
           <button className="btn btn-danger">全てのログを削除する</button>
         </form>
@@ -33,12 +50,11 @@ const App = () =>  {
             </tr>
           </thead>
           <tbody>
-
           </tbody>
         </table>
       </div>
     </>
-  );
+  )
 }
 
 export default App
