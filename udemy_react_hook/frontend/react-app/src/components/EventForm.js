3e2631e29ad1becadc630d1572f616cacf6086b3
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-29 19:16:55 +0900

    delete all operation logs

--- udemy_react_hook/frontend/react-app/src/components/EventForm.js
+++ udemy_react_hook/frontend/react-app/src/components/EventForm.js
@@ -47,6 +47,14 @@ const EventForm = () => {
 
   const unCreatable = title === '' || body === ''
 
+  const deleteAllOperationLogs = e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除してもよいですか？')
+    if(result) {
+      dispatch({ type: DELETE_ALL_OPERATION_LOGS })
+    }
+  }
+
   return (
     <>
       <h4>イベント作成フォーム</h4>
@@ -63,7 +71,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
-        <button className="btn btn-danger">全てのログを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全てのログを削除する</button>
       </form>
     </>
   )
