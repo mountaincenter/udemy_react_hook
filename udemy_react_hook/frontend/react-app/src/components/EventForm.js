6e2ddce2800f3d9b9188530b08fd67394a0fe825
mountaincenter <y.hiroyuki560730@gmail.com>
2022-09-29 19:04:02 +0900

    create operation logs

--- udemy_react_hook/frontend/react-app/src/components/EventForm.js
+++ udemy_react_hook/frontend/react-app/src/components/EventForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext } from "react"
 import {
   CREATE_EVENT,
-  DELETE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
 } from "../actions"
 
 import AppContext from "../contexts/AppContext"
+import { timeCurrentIso8601 } from "../utils"
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
@@ -20,6 +22,12 @@ const EventForm = () => {
       body
     })
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました',
+      operatedAt: timeCurrentIso8601
+    })
+
     setTitle('')
     setBody('')
   }
@@ -27,7 +35,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault() //ボタンクリック時には入れる
     const result = window.confirm('全てのイベントを本当に削除してもよいですか？')
-    if (result) dispatch({ type: DELETE_ALL_EVENTS })
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました',
+        operatedAt: timeCurrentIso8601
+      })
+    }
   }
 
   const unCreatable = title === '' || body === ''
